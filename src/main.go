66b31e0c7fbf38e57eec4a231429d539344b5096
Aleksandar Ignjatijevic <a.ignjatijevic@gmail.com>
2021-06-03 01:19:12 +0200

    :construction: Kinda fixed cors :|

--- src/main.go
+++ src/main.go
@@ -5,9 +5,6 @@ import (
 	"FollowService/infrastructure/mongo"
 	"FollowService/infrastructure/seeder"
 	"FollowService/interactor"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func main() {
@@ -18,24 +15,46 @@ func main() {
 
 	i := interactor.NewInteractor(mongoCli)
 	appHandler := i.NewAppHandler()
+
 	g := router.NewRouter(appHandler)
 
 	//g := gin.Default()
-	g.Use(gin.Logger())
-	g.Use(gin.Recovery())
+	//g.Use(gin.Logger())
+	//g.Use(gin.Recovery())
 
 	//TODO: check about possible changes in middleware
-	g.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE", "HEAD"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		//AllowOriginFunc: func(origin string) bool {
-		//	return origin == "https://github.com"
-		//},
-		MaxAge: 12 * time.Hour,
-	}))
+	//g.Use(cors.New(cors.Config{
+	//	AllowOrigins:     []string{"*"},
+	//	AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE", "HEAD"},
+	//	AllowHeaders:     []string{"Origin"},
+	//	ExposeHeaders:    []string{"Content-Length"},
+	//	AllowCredentials: true,
+	//	AllowOriginFunc: func(origin string) bool {
+	//		return origin == "*"
+	//	},
+	//	MaxAge: 12 * time.Hour,
+	//}))
+	//g.Use(cors.Default())
+	//g.Use(CORSMiddleware())
+	//g.Use(func(ctx *gin.Context) {
+	//	ctx.Header("Access-Control-Allow-Origin", "*")
+	//})
+
+	//g.Use(func(ctx *gin.Context) {
+	//	ctx.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	//})
+
+	//corsConfig := cors.DefaultConfig()
+	//
+	//corsConfig.AllowOrigins = []string{"https://example.com"}
+	//// To be able to send tokens to the server.
+	//corsConfig.AllowCredentials = true
+	//
+	//// OPTIONS method for ReactJS
+	//corsConfig.AddAllowMethods("OPTIONS", "POST")
+	//
+	//// Register the middleware
+	//g.Use(cors.New(corsConfig))
 
 	//todo: check this middleware thingy out
 	//g.Use(static.Serve("/static", static.LocalFile("/assets", false)))
